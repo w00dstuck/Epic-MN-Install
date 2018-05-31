fca92d86baf9e74cf584ff2470e7be4e956f3aa2
manfromafar <jonsonb10@gmail.com>
2018-05-31 17:30:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -227,7 +227,7 @@ if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
   ufw default deny incoming
   ufw default allow outgoing
   ufw allow ssh
-  ufw allow 52543/tcp
+  ufw allow 6942/tcp
   yes | ufw enable
 fi
 
