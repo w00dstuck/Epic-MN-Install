3ee46f53a8c8e5b3cba3aeffea6901c227eee851
manfromafar <jonsonb10@gmail.com>
2018-05-31 17:35:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -299,19 +299,7 @@ WantedBy=multi-user.target
 EOL
 sudo systemctl enable northern.service
 sudo systemctl start northern.service
-EOL
 
-echo "Your masternode is syncing. Please wait for this process to finish."
-echo "This can take up to a few hours. Do not close this window." && echo ""
-
-until su -c "northern-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" $USER; do
-  for (( i=0; i<${#CHARS}; i++ )); do
-    sleep 2
-    echo -en "${CHARS:$i:1}" "\r"
-  done
-done
-
-clear
 
 cat << EOL
 
