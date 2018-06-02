9ad95a6acc7fd51e12fab82bf679078548adad5f
zabtc <37597405+zabtc@users.noreply.github.com>
2018-06-02 16:39:49 +0200

    Update StakingSetupVPS.sh

--- StakingSetupVPS.sh
+++ StakingSetupVPS.sh
@@ -27,8 +27,8 @@ sudo free
 sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 cd
 
- wget https://github.com/zabtc/Northern/releases/download/1.0.0/northern-1.0.0-x86_64-linux-gnu.tar.gz
- tar -xzf Northern*.tar.gz
+wget https://github.com/zabtc/Northern/releases/download/1.0.0/northern-1.0.0-x86_64-linux-gnu.tar.gz
+tar -xzf northern-1.0.0-x86_64-linux-gnu.tar.gz
 
 sudo apt-get install -y ufw
 sudo ufw allow ssh/tcp
